a14edcc0ed0ac7aae8d6b8a97ab3e4c3944fa466
Krzysztof Kisiel <qminec@gmail.com>
2022-03-11 18:59:08 +0100

    server add

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,27 +1,19 @@
 import React from 'react'
-import {Form, Button} from 'react-bootstrap'
+import {Form, Button, Container} from 'react-bootstrap'
 
 export default function Login() {
-  return (
-    <Form>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
 
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
+
+
+  return (
+<Container>
+<Form>
+ <Form.Group>
+    <Form.Label>Chose your ID</Form.Label>
+    <Form.Control type="text" required></Form.Control>
+ <Button>Login</Button>
+ </Form.Group>
 </Form>
+</Container>
   )
 }
